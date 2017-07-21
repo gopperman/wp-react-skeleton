eade95eb09f80d79f55ff082fee65d1ff5c63a54
Greg Opperman <gopperman@gmail.com>
2017-07-21 11:14:49 -0400

    Detect if react app exists, fail gracefully

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
  */
  
 // Change this variable to be the name of the app you generated with create-react-app
-$appName = 'app';
+$appPath = './app/build/index.html';
 
-include $appName . '/build/index.html'; 
+if ( file_exists($appPath) ) {
+	include $appPath;
+} else { ?>
+	<h1>wp-react-skeleton</h1>
+	<p>Hi! Looks like there's no react app here yet. Install and run 'create-react-app' in this directory, then 'npm build' your app to get started.</p>
+	<p>If you called your app something besides 'app', edit $appPath in index.php</p>
+	<p>Happy hacking!</p>
+<?php }
